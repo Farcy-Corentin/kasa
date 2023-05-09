cf9fe65ef9118fc6ff08b317a0d9c19e2248354e
corentin <corentinfarcy1@gmail.com>
2023-05-09 17:26:41 +0200

    fix(KAS14): add padding and grey background to desktop properties section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,7 @@
 import React from 'react'
 import './sass/main.scss'
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
-import { About } from './pages/About'
-import { Home } from './pages/Home'
-import { Property } from './pages/Property'
-import { Layout } from './layout/Layout'
-import { RootErrorBoundary } from './libs/Routes'
-
-const rooter = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <RootErrorBoundary />,
-    children: [
-      {
-        path: '',
-        element: <Outlet />,
-        children: [
-          {
-            path: '',
-            element: <Home />,
-          },
-          {
-            path: 'about',
-            element: <About />,
-          },
-          {
-            path: 'property/:id',
-            element: <Property />,
-          },
-        ],
-      },
-    ],
-  },
-])
+import { RouterProvider } from 'react-router-dom'
+import { rooter } from './libs/Routes'
 
 function App(): JSX.Element {
   return (
--- src/libs/Routes.tsx
+++ src/libs/Routes.tsx
@@ -1,8 +1,39 @@
-import { useRouteError } from 'react-router-dom'
+import { createBrowserRouter, Outlet, useRouteError } from 'react-router-dom'
 import { Error as ErrorPage } from '../pages/Error'
 import React from 'react'
 import type { IError } from '../interfaces/IError'
+import { Layout } from '../layout/Layout'
+import { Home } from '../pages/Home'
+import { About } from '../pages/About'
+import { Property } from '../pages/Property'
 
+export const rooter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <RootErrorBoundary />,
+    children: [
+      {
+        path: '',
+        element: <Outlet />,
+        children: [
+          {
+            path: '',
+            element: <Home />,
+          },
+          {
+            path: 'about',
+            element: <About />,
+          },
+          {
+            path: 'property/:id',
+            element: <Property />,
+          },
+        ],
+      },
+    ],
+  },
+])
 export function RootErrorBoundary(): JSX.Element {
   const error = useRouteError() as IError
 
